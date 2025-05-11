29603e19a703ec3469cd8ea5b9ef7bdd9407a27a
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-05-11 00:32:57 -0400

    Finish 3.3-3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -33,6 +33,22 @@ app.get("/info", (req, res) => {
   res.send(`Phonebook has info for ${entries.length} people<br/> ${Date()}`);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const entry = entries.find(entry => entry.id === req.params.id);
+
+  if (entry) {
+    res.json(entry);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  entries = entries.filter(entry => entry.id !== req.params.id);
+
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
