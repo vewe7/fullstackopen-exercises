4d6bd6563d4dacefd31a956eb8e4ef18319fef65
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-05-18 16:08:30 -0400

    Finish 3.7-3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 const ID_MAX = 8000000000;
 
