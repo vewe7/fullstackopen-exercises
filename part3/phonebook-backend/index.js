1424b1d33385797de7c071d5561dcbabefb75ec4
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-05-24 16:43:57 -0400

    Finish 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:5173"
+};
+
+app.use(cors(corsOptions));
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
