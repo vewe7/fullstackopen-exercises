025b48e52b60d7ff36be71d665b1bbad76367d45
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-05-24 17:16:32 -0400

    Remove cors middleware from phonebook backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static("dist"));
-
-const corsOptions = {
-  origin: "http://localhost:5173"
-};
-
-app.use(cors(corsOptions));
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
