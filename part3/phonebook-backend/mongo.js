2447eced8785cbf25dcf7a279134034cb5ed8da5
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-05-27 17:14:38 -0400

    Finish 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("password missing from arguments");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://nyoon:${password}@cluster0.dzqayjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+
+
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Entry = mongoose.model("Entry", entrySchema);
+
+
+
+if (process.argv.length < 5) {
+  Entry.find({}).then(result => {
+    console.log("phonebook:");
+
+    result.forEach(entry => {
+      console.log(`${entry.name} ${entry.number}`);
+    });
+    mongoose.connection.close();
+  });
+
+} else {
+  const entry = new Entry({
+    name: process.argv[3],
+    number: process.argv[4]
+  });
+
+  entry.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+
