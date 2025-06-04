7b868150cb23d193bbc8fa387f4d2cbdca77a7b9
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-06-04 19:23:33 -0400

    Finish 3.15

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,37 +6,11 @@ const Entry = require("./models/entry");
 
 const app = express();
 
-app.use(express.json());
-
 app.use(express.static("dist"));
+app.use(express.json());
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
-
-const ID_MAX = 8000000000;
-
-let entries = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
 
 app.get("/api/persons", (req, res) => {
   Entry.find({}).then(entries => {
@@ -48,16 +22,24 @@ app.get("/info", (req, res) => {
   res.send(`Phonebook has info for ${entries.length} people<br/> ${Date()}`);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Entry.findById(req.params.id).then(entry => {
-    res.json(entry);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Entry.findById(req.params.id)
+    .then(entry => {
+      if (entry) {
+        res.json(entry);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  entries = entries.filter(entry => entry.id !== req.params.id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Entry.findByIdAndDelete(req.params.id)
+    .then(entry => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -78,6 +60,17 @@ app.post("/api/persons", (req, res) => {
     res.json(savedEntry);
   });
 });
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError")
+    return res.status(400).send({ error: "malformatted id" });
+
+  next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
