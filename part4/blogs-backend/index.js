d924ba52544eb0b27796c6ea871baec42bab3b0c
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-06-16 17:06:18 -0400

    Finish 4.2

--- part4/blogs-backend/index.js
+++ part4/blogs-backend/index.js
@@ -1,38 +1,17 @@
-require("dotenv").config();
-
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(config.MONGODB_URL);
 
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
