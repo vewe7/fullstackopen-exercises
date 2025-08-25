756af34e3f58f28720a5576f482d215f3261067e
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-08-25 14:14:50 -0500

    Finish 4.5

--- part4/blogs-backend/utils/list_helper.js
+++ part4/blogs-backend/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length == 0)
+    return null;
+
+  return blogs.reduce((prev, blog) =>
+    prev.likes < blog.likes ? blog : prev, blogs[0]);
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
