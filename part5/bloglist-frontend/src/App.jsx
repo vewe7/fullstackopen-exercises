a47d63bc7582124e1377b8c8de758e0b06053c86
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-09-19 13:08:50 -0500

    Finish 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -17,11 +17,25 @@ const App = () => {
     )
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      // blogService.setToken(user.token);
+    }
+  }, []);
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
       const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem(
+        "loggedBlogappUser", JSON.stringify(user)
+      );
+
       setUser(user);
       setUsername("");
       setPassword("");
@@ -31,6 +45,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000);
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogappUser");
+    setUser(null);
   }
 
   if (user === null) {
@@ -70,6 +89,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+      <button type="button" onClick={handleLogout}>logout</button>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
