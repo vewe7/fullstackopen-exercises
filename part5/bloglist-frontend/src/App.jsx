7ce8ae08ebf3d55d65be88b6d2e29443c8624732
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-09-26 02:41:04 -0500

    Finish 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
         />
       </Togglable>
 
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
       )}
     </div>
