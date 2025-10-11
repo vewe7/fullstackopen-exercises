9c53b1d61dda5bebe3f8e6d5b8c85ab49eca831a
Naomi Yoon <naomi.yoon.do@gmail.com>
2025-10-11 14:57:54 -0500

    Finish 6.20

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,51 @@
+import AnecdoteForm from "./components/AnecdoteForm"
+import Notification from "./components/Notification"
+import { useQuery } from "@tanstack/react-query";
+import { getAnecdotes } from "./requests";
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log("vote")
+  }
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: 1
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>Loading data...</div>;
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to server issue</div>;
+  }
+
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
